84d9220c2a5a7851c27420310da1a854eb8357ad
Charlotte <925433739@qq.com>
2015-10-11 17:44:07 +0800

    first.js

---
+++ first.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("img.one").fadeIn(8000);
+    $("img.two").fadeIn(8000);
+    $("img.three").fadeIn(8000);
+    $("img.four").fadeIn(8000);
+    $("p").fadeIn(4000);
+    $("p").fadeIn(4000);
+});
